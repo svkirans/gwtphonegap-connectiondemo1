3427ac562db369a980956804f482f223daff21f0
svkirans <svkirans@gmail.com>
2016-05-03 18:57:29 +0530

    Commit for working around the issue with online event being fired multiple times in network api.

---
+++ gwtphonegap-connectiondemo1/android/ConnectionDemo1/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-network-information": "1.2.1",
+    "cordova-plugin-whitelist": "1.2.2"
+};
+// BOTTOM OF METADATA
+});
